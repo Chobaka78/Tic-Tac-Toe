a169b906a88cb1e50558051fe44c3a856f47c20b
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2020-01-27 11:30:15 -0500

    Upload 1

---
+++ cmake-build-debug/CMakeFiles/Tic_Tac_Toe_Assignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tic_Tac_Toe_Assignment.dir/main.c.obj"
+  "Tic_Tac_Toe_Assignment.exe"
+  "Tic_Tac_Toe_Assignment.exe.manifest"
+  "Tic_Tac_Toe_Assignment.pdb"
+  "libTic_Tac_Toe_Assignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tic_Tac_Toe_Assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
